c141c8cf66fca8be91a792113ef1cf000cfe284e
unknown <sergiy.vakulenko@gmail.com>
2016-05-01 00:09:52 +0300

    Add skeleton for cam10 0.5 ascom driver

---
+++ cam10_v05/cam10_v05/camSettings.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ASCOM.cam10_v05
+{
+    partial class camSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gainTrackBar = new System.Windows.Forms.TrackBar();
+            this.offsetTrackBar = new System.Windows.Forms.TrackBar();
+            this.minMaxOffsetLabel = new System.Windows.Forms.Label();
+            this.minMaxGainLabel = new System.Windows.Forms.Label();
+            this.offsetLabel = new System.Windows.Forms.Label();
+            this.gainLabel = new System.Windows.Forms.Label();
+            this.onTopCheckBox = new System.Windows.Forms.CheckBox();
+            this.blevelLabel = new System.Windows.Forms.Label();
+            this.minMaxBlevelLabel = new System.Windows.Forms.Label();
+            this.blevelTrackBar = new System.Windows.Forms.TrackBar();
+            this.gainNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.offsetNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.blevelNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.autoOffsetCheckBox = new System.Windows.Forms.CheckBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.histStretchUpDown = new System.Windows.Forms.NumericUpDown();
+            this.histogramStretchlabel = new System.Windows.Forms.Label();
+            this.cameraStatusLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gainTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blevelTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gainNumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetNumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blevelNumUpDown)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histStretchUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gainTrackBar
+            // 
+            this.gainTrackBar.Location = new System.Drawing.Point(3, 22);
+            this.gainTrackBar.Maximum = 63;
+            this.gainTrackBar.Name = "gainTrackBar";
+            this.gainTrackBar.Size = new System.Drawing.Size(219, 45);
+            this.gainTrackBar.TabIndex = 36;
+            this.gainTrackBar.Scroll += new System.EventHandler(this.GainTrackBar_Scroll);
+            // 
+            // offsetTrackBar
+            // 
+            this.offsetTrackBar.Location = new System.Drawing.Point(3, 88);
+            this.offsetTrackBar.Maximum = 63;
+            this.offsetTrackBar.Minimum = -63;
+            this.offsetTrackBar.Name = "offsetTrackBar";
+            this.offsetTrackBar.Size = new System.Drawing.Size(219, 45);
+            this.offsetTrackBar.TabIndex = 35;
+            this.offsetTrackBar.Scroll += new System.EventHandler(this.OffsetTrackBar_Scroll);
+            // 
+            // minMaxOffsetLabel
+            // 
+            this.minMaxOffsetLabel.AutoSize = true;
+            this.minMaxOffsetLabel.Location = new System.Drawing.Point(44, 70);
+            this.minMaxOffsetLabel.Name = "minMaxOffsetLabel";
+            this.minMaxOffsetLabel.Size = new System.Drawing.Size(40, 13);
+            this.minMaxOffsetLabel.TabIndex = 34;
+            this.minMaxOffsetLabel.Text = "-63..63";
+            // 
+            // minMaxGainLabel
+            // 
+            this.minMaxGainLabel.AutoSize = true;
+            this.minMaxGainLabel.Location = new System.Drawing.Point(44, 4);
+            this.minMaxGainLabel.Name = "minMaxGainLabel";
+            this.minMaxGainLabel.Size = new System.Drawing.Size(31, 13);
+            this.minMaxGainLabel.TabIndex = 33;
+            this.minMaxGainLabel.Text = "0..63";
+            // 
+            // offsetLabel
+            // 
+            this.offsetLabel.AutoSize = true;
+            this.offsetLabel.Location = new System.Drawing.Point(3, 70);
+            this.offsetLabel.Name = "offsetLabel";
+            this.offsetLabel.Size = new System.Drawing.Size(35, 13);
+            this.offsetLabel.TabIndex = 32;
+            this.offsetLabel.Text = "Offset";
+            // 
+            // gainLabel
+            // 
+            this.gainLabel.AutoSize = true;
+            this.gainLabel.Location = new System.Drawing.Point(3, 4);
+            this.gainLabel.Name = "gainLabel";
+            this.gainLabel.Size = new System.Drawing.Size(29, 13);
+            this.gainLabel.TabIndex = 29;
+            this.gainLabel.Text = "Gain";
+            // 
+            // onTopCheckBox
+            // 
+            this.onTopCheckBox.AutoSize = true;
+            this.onTopCheckBox.Location = new System.Drawing.Point(160, 4);
+            this.onTopCheckBox.Name = "onTopCheckBox";
+            this.onTopCheckBox.Size = new System.Drawing.Size(62, 17);
+            this.onTopCheckBox.TabIndex = 37;
+            this.onTopCheckBox.Text = "On Top";
+            this.onTopCheckBox.UseVisualStyleBackColor = true;
+            this.onTopCheckBox.CheckedChanged += new System.EventHandler(this.OnTopCheckBox_CheckedChanged);
+            // 
+            // blevelLabel
+            // 
+            this.blevelLabel.AutoSize = true;
+            this.blevelLabel.Location = new System.Drawing.Point(3, 135);
+            this.blevelLabel.Name = "blevelLabel";
+            this.blevelLabel.Size = new System.Drawing.Size(36, 13);
+            this.blevelLabel.TabIndex = 38;
+            this.blevelLabel.Text = "Blevel";
+            // 
+            // minMaxBlevelLabel
+            // 
+            this.minMaxBlevelLabel.AutoSize = true;
+            this.minMaxBlevelLabel.Location = new System.Drawing.Point(44, 135);
+            this.minMaxBlevelLabel.Name = "minMaxBlevelLabel";
+            this.minMaxBlevelLabel.Size = new System.Drawing.Size(37, 13);
+            this.minMaxBlevelLabel.TabIndex = 39;
+            this.minMaxBlevelLabel.Text = "0..255";
+            // 
+            // blevelTrackBar
+            // 
+            this.blevelTrackBar.Location = new System.Drawing.Point(3, 155);
+            this.blevelTrackBar.Maximum = 255;
+            this.blevelTrackBar.Name = "blevelTrackBar";
+            this.blevelTrackBar.Size = new System.Drawing.Size(219, 45);
+            this.blevelTrackBar.TabIndex = 41;
+            this.blevelTrackBar.Scroll += new System.EventHandler(this.BlevelTrackBar_Scroll);
+            // 
+            // gainNumUpDown
+            // 
+            this.gainNumUpDown.Location = new System.Drawing.Point(90, 2);
+            this.gainNumUpDown.Maximum = new decimal(new int[] {
+            63,
+            0,
+            0,
+            0});
+            this.gainNumUpDown.Name = "gainNumUpDown";
+            this.gainNumUpDown.Size = new System.Drawing.Size(51, 20);
+            this.gainNumUpDown.TabIndex = 42;
+            this.gainNumUpDown.ValueChanged += new System.EventHandler(this.gainNumUpDown_ValueChanged);
+            // 
+            // offsetNumUpDown
+            // 
+            this.offsetNumUpDown.Location = new System.Drawing.Point(90, 68);
+            this.offsetNumUpDown.Maximum = new decimal(new int[] {
+            63,
+            0,
+            0,
+            0});
+            this.offsetNumUpDown.Minimum = new decimal(new int[] {
+            63,
+            0,
+            0,
+            -2147483648});
+            this.offsetNumUpDown.Name = "offsetNumUpDown";
+            this.offsetNumUpDown.Size = new System.Drawing.Size(51, 20);
+            this.offsetNumUpDown.TabIndex = 43;
+            this.offsetNumUpDown.ValueChanged += new System.EventHandler(this.offsetNumUpDown_ValueChanged);
+            // 
+            // blevelNumUpDown
+            // 
+            this.blevelNumUpDown.Location = new System.Drawing.Point(90, 133);
+            this.blevelNumUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.blevelNumUpDown.Name = "blevelNumUpDown";
+            this.blevelNumUpDown.Size = new System.Drawing.Size(51, 20);
+            this.blevelNumUpDown.TabIndex = 44;
+            this.blevelNumUpDown.ValueChanged += new System.EventHandler(this.blevelNumUpDown_ValueChanged);
+            // 
+            // autoOffsetCheckBox
+            // 
+            this.autoOffsetCheckBox.AutoSize = true;
+            this.autoOffsetCheckBox.Location = new System.Drawing.Point(160, 70);
+            this.autoOffsetCheckBox.Name = "autoOffsetCheckBox";
+            this.autoOffsetCheckBox.Size = new System.Drawing.Size(48, 17);
+            this.autoOffsetCheckBox.TabIndex = 45;
+            this.autoOffsetCheckBox.Text = "Auto";
+            this.autoOffsetCheckBox.UseVisualStyleBackColor = true;
+            this.autoOffsetCheckBox.CheckedChanged += new System.EventHandler(this.autoOffsetCheckBox_CheckedChanged);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.histStretchUpDown);
+            this.panel.Controls.Add(this.histogramStretchlabel);
+            this.panel.Controls.Add(this.cameraStatusLabel);
+            this.panel.Controls.Add(this.gainLabel);
+            this.panel.Controls.Add(this.autoOffsetCheckBox);
+            this.panel.Controls.Add(this.offsetLabel);
+            this.panel.Controls.Add(this.blevelNumUpDown);
+            this.panel.Controls.Add(this.minMaxGainLabel);
+            this.panel.Controls.Add(this.offsetNumUpDown);
+            this.panel.Controls.Add(this.minMaxOffsetLabel);
+            this.panel.Controls.Add(this.gainNumUpDown);
+            this.panel.Controls.Add(this.offsetTrackBar);
+            this.panel.Controls.Add(this.blevelTrackBar);
+            this.panel.Controls.Add(this.gainTrackBar);
+            this.panel.Controls.Add(this.minMaxBlevelLabel);
+            this.panel.Controls.Add(this.onTopCheckBox);
+            this.panel.Controls.Add(this.blevelLabel);
+            this.panel.Location = new System.Drawing.Point(2, 1);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(228, 239);
+            this.panel.TabIndex = 46;
+            // 
+            // histStretchUpDown
+            // 
+            this.histStretchUpDown.Location = new System.Drawing.Point(122, 194);
+            this.histStretchUpDown.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.histStretchUpDown.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.histStretchUpDown.Name = "histStretchUpDown";
+            this.histStretchUpDown.Size = new System.Drawing.Size(46, 20);
+            this.histStretchUpDown.TabIndex = 49;
+            this.histStretchUpDown.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.histStretchUpDown.ValueChanged += new System.EventHandler(this.histStretchUpDown_ValueChanged);
+            // 
+            // histogramStretchlabel
+            // 
+            this.histogramStretchlabel.AutoSize = true;
+            this.histogramStretchlabel.Location = new System.Drawing.Point(3, 197);
+            this.histogramStretchlabel.Name = "histogramStretchlabel";
+            this.histogramStretchlabel.Size = new System.Drawing.Size(113, 13);
+            this.histogramStretchlabel.TabIndex = 47;
+            this.histogramStretchlabel.Text = "Histogram Stretch, bits";
+            // 
+            // cameraStatusLabel
+            // 
+            this.cameraStatusLabel.AutoSize = true;
+            this.cameraStatusLabel.Location = new System.Drawing.Point(3, 219);
+            this.cameraStatusLabel.Name = "cameraStatusLabel";
+            this.cameraStatusLabel.Size = new System.Drawing.Size(132, 13);
+            this.cameraStatusLabel.TabIndex = 46;
+            this.cameraStatusLabel.Text = "Camera status: operational";
+            // 
+            // camSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 241);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "camSettings";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "CAM10_settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.camSettings_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.gainTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blevelTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gainNumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.offsetNumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blevelNumUpDown)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histStretchUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar gainTrackBar;
+        private System.Windows.Forms.TrackBar offsetTrackBar;
+        private System.Windows.Forms.Label minMaxOffsetLabel;
+        private System.Windows.Forms.Label minMaxGainLabel;
+        private System.Windows.Forms.Label offsetLabel;
+        private System.Windows.Forms.Label gainLabel;
+        private System.Windows.Forms.CheckBox onTopCheckBox;
+        private System.Windows.Forms.Label blevelLabel;
+        private System.Windows.Forms.Label minMaxBlevelLabel;
+        private System.Windows.Forms.TrackBar blevelTrackBar;
+        private System.Windows.Forms.NumericUpDown gainNumUpDown;
+        private System.Windows.Forms.NumericUpDown offsetNumUpDown;
+        private System.Windows.Forms.NumericUpDown blevelNumUpDown;
+        private System.Windows.Forms.CheckBox autoOffsetCheckBox;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label cameraStatusLabel;
+        private System.Windows.Forms.NumericUpDown histStretchUpDown;
+        private System.Windows.Forms.Label histogramStretchlabel;
+    }
+}
---
+++ cam10ll05/cam10ll05.dpr
@@ -0,0 +1,367 @@
+// --------------------------------------------------------------------------------
+// ASCOM Camera driver low-level interaction library for cam10_v0.1
+// Edit Log:
+// Date		    	Who	 Vers	  Description
+// -----------	---	-----	---------------------------------------------------------
+// 17-feb-2015  VSS 0.1     Initial
+// 25-mar-2015  VSS 0.2     Fixed bug with memory leak with hev event, enhanced frame reading procedure
+// 18-apr-2015  VSS 0.3     FT_Read, FT_Write error handling
+// 01-nov-2015  VSS 0.4     Only version changed
+// 30-apr-2016  VSS 0.5     Oversampling introduced
+// --------------------------------------------------------------------------------
+
+library cam10ll05;
+
+uses
+  MyD2XX,
+  Classes,
+  SysUtils,
+  Windows,
+  SyncObjs;
+
+{$R *.res}
+
+type
+  posl = class(TThread)
+  private
+    { Private declarations }
+  protected
+    procedure Execute; override;
+  end;
+
+const
+portfirst  = $f9;
+CameraWidth = 1280;
+CameraHeight = 1024;
+razm = 50;
+
+//camera state consts
+cameraIdle = 0;
+cameraWaiting = 1;
+cameraExposing = 2;
+cameraReading = 3;
+cameraDownload = 4;
+cameraError = 5;
+
+//GLobal variables}
+var
+adress:integer;
+co: posl;
+bufim:array[0..(CameraWidth)*CameraHeight-1] of byte;
+bufim2:array[0..CameraWidth*CameraHeight-1] of word;
+sdx,sy0,sdy:integer;
+kolbyte:integer;
+blevel:byte;
+hev:TEvent;
+bufa:integer;
+zad:integer;
+kbyte:integer;
+//time to wait for frame
+dlit1:integer;
+
+//flag, camera connection state
+isConnected : boolean = false;
+//flag, image ready state
+imageReady : boolean = false;
+cameraState : integer = cameraIdle;
+//error Flag
+errorReadFlag : boolean;
+errorWriteFlag : boolean;
+
+procedure writp;
+begin
+  if (not errorWriteFlag) then errorWriteFlag:=Write_USB_Device_Buffer_wErr(FT_CAM10B,adress);
+  adress:=0;
+end;
+
+//reading image from sensor
+procedure posl.Execute;
+var
+x,y,buf:integer;
+begin
+  cameraState:=cameraReading;
+  sleep(zad);
+  bufa:=0;
+  buf:=0;
+  if (not errorWriteFlag) then buf:=Read_USB_Device_Buffer(FT_CAM10A,@bufim,kolbyte);
+  kbyte:=buf;
+  if buf = kolbyte then
+    for y:=0 to sdy-1 do
+      for x:=0 to sdx-1 do
+        bufim2[x+CameraWidth*(y+sy0)]:=bufim[x+y*(sdx+0)]
+  else begin
+    bufa:=1;
+    errorReadFlag:=true;
+  end;
+  if (not errorWriteFlag) then
+  begin
+    Get_USB_Device_Status(FT_CAM10A);
+    if FT_Q_Bytes > 0 then Purge_USB_Device(FT_CAM10A,FT_PURGE_RX);
+  end;
+  hev.SetEvent;
+  cameraState:=cameraDownload;
+  cameraState:=cameraIdle;
+  imageReady:=true;
+end;
+
+procedure ComRead;
+begin
+  co:=posl.Create(true);
+  co.FreeOnTerminate:=true;
+  co.Priority:=tpNormal;
+  co.Resume;
+end;
+
+procedure starti;
+begin
+   FT_Out_Buffer[adress+0]:=portfirst or $2;
+   FT_Out_Buffer[adress+1]:=portfirst or $6;
+   FT_Out_Buffer[adress+2]:=portfirst or $4;
+   inc(adress,3);
+end;
+
+procedure bytei(val:byte);
+var i:integer;
+buf:byte;
+begin
+  for i:=0 to 7 do
+  begin
+    if (val and $80) <> 0 then buf:=portfirst
+    else buf:=portfirst or $4;
+    val:=val shl 1;
+    FT_Out_Buffer[adress+0]:=buf;
+    FT_Out_Buffer[adress+1]:=buf or $2;
+    FT_Out_Buffer[adress+2]:=buf;
+    inc(adress,3);
+  end;
+  FT_Out_Buffer[adress+0]:=portfirst;
+  FT_Out_Buffer[adress+1]:=portfirst or $2;
+  FT_Out_Buffer[adress+2]:=portfirst or $4;
+  inc(adress,3);
+end;
+
+procedure stopi;
+begin
+   FT_Out_Buffer[adress+0]:=portfirst or $4;
+   FT_Out_Buffer[adress+1]:=portfirst or $6;
+   FT_Out_Buffer[adress+2]:=portfirst or $2;
+   FT_Out_Buffer[adress+3]:=portfirst;
+   inc(adress,4);
+end;
+
+procedure writes(adr:byte;val:word);
+begin
+  adress:=0;
+  starti;
+  bytei($ba);
+  bytei(adr);
+  bytei(hi(val));
+  bytei(lo(val));
+  stopi;
+  writp;
+end;
+
+function readframe(x0,dx,y0,dy:integer):boolean;
+begin
+  writes($01,12+y0);
+  writes($03,dy-1);
+  writes($0b,$1);
+  kolbyte:=dx*dy;
+  sdx:=dx;
+  sdy:=dy;
+  sy0:=y0;
+  ComRead;
+  hev.WaitFor(dlit1);
+  if bufa < 1 then result:=true
+  else begin
+    if (not errorWriteFlag) then
+    begin
+      Purge_USB_Device(FT_CAM10A, FT_PURGE_RX or FT_PURGE_TX);
+      Purge_USB_Device(FT_CAM10B, FT_PURGE_RX or FT_PURGE_TX);
+    end;
+    result:=false;
+  end;
+end;
+
+function QueueStatus:integer;
+begin
+  if (not errorWriteFlag) then Get_USB_Device_QueueStatus(FT_CAM10A);
+  result:=FT_Q_Bytes;
+end;
+
+//Set camera gain, return bool result
+function cameraSetGain (val : integer) : WordBool;
+begin
+  writes($35,2*val);
+  Result :=true;
+end;
+
+//Set camera offset, return bool result
+function cameraSetOffset (val : smallint;aut:boolean) : WordBool;
+begin
+  writes($60,2*val);
+  writes($61,2*val);
+  writes($63,2*val+blevel);
+  writes($64,2*val+blevel);
+  if aut then writes($62,$0498)
+  else writes($62,$049d);
+  Result :=true;
+end;
+
+procedure resetchip;
+begin
+  adress:=0;
+  for adress:=0 to 99 do
+    FT_Out_Buffer[adress]:=portfirst;
+  for adress:=100 to 199 do
+    FT_Out_Buffer[adress]:=portfirst - $1;
+  for adress:=200 to 299 do
+    FT_Out_Buffer[adress]:=portfirst;
+  writp;
+end;
+
+procedure byteo(val:byte);
+var i:integer;
+b,buf:byte;
+begin
+  b:=$fe;
+  for i:=0 to 7 do
+  begin
+    if (b and $80) <> 0 then buf:=portfirst
+    else buf:=portfirst+$4;
+    b:=2*b;
+    FT_Out_Buffer[adress+0]:=portfirst;
+    FT_Out_Buffer[adress+1]:=portfirst or $2;
+    FT_Out_Buffer[adress+2]:=buf;
+    inc(adress,3);
+  end;
+  FT_Out_Buffer[adress+0]:=portfirst or val;
+  FT_Out_Buffer[adress+1]:=portfirst or (val or $2);
+  FT_Out_Buffer[adress+2]:=portfirst or val;
+  inc(adress,3);
+end;
+
+function cameraConnect () : WordBool;  stdcall; export;
+var  FT_OP_flag : boolean;
+begin
+  FT_OP_flag:=true;
+  FT_Enable_Error_Report:=false;
+  errorWriteFlag:=false;
+  if (FT_OP_flag) then
+    begin
+      if Open_USB_Device_By_Serial_Number(FT_CAM10A,'CAM10A') <> FT_OK then FT_OP_flag := false;
+    end;
+  if (FT_OP_flag) then
+    begin
+      if Open_USB_Device_By_Serial_Number(FT_CAM10B,'CAM10B') <> FT_OK then FT_OP_flag := false;
+    end;
+  if (FT_OP_flag) then
+    begin
+      // BitMode
+      if Set_USB_Device_BitMode(FT_CAM10B,$f7, $04)  <> FT_OK then FT_OP_flag := false;
+    end;
+  if (FT_OP_flag) then
+    begin
+      FT_Current_Baud:=100000;
+      Set_USB_Device_BaudRate(FT_CAM10B);
+      Set_USB_Device_LatencyTimer(FT_CAM10B,2);
+      Set_USB_Device_LatencyTimer(FT_CAM10A,2);
+      Set_USB_Device_TimeOuts(FT_CAM10A,250,100);
+      resetchip;
+      writes($1e,$8100);
+      writes($20,$0104);
+      CameraSetGain (63);
+      writes($60,0);
+      writes($61,0);
+      writes($63,blevel);
+      writes($64,blevel);
+      writes($62,$049d);
+    end;
+  isConnected := FT_OP_flag;
+  errorReadFlag := false;
+  cameraState := cameraIdle;
+  if(FT_OP_flag=false) then cameraState := cameraError;
+  Result := isConnected;
+end;
+
+//Disconnect camera, return bool result
+function cameraDisconnect (): WordBool; stdcall; export;
+var FT_OP_flag : boolean;
+begin
+  FT_OP_flag := true;
+  if Close_USB_Device(FT_CAM10A) <> FT_OK then FT_OP_flag := false;
+  if Close_USB_Device(FT_CAM10B) <> FT_OK then FT_OP_flag := false;
+  if (FT_OP_flag=false) then cameraState := cameraError
+  else cameraState := cameraIdle;
+  IsConnected := not FT_OP_flag;
+  Result:= FT_OP_flag;
+end;
+
+//return camera connection status
+function cameraIsConnected () : WordBool; stdcall; export;
+begin
+  Result := isConnected;
+end;
+
+//starts exposure
+function cameraStartExposure (startY,numY : integer; duration : double; gain,offset : integer; autoOffset : WordBool; sblevel : integer) : WordBool; stdcall; export;
+var x:integer;
+begin
+  imageReady := false;
+  errorReadFlag := false;
+  cameraState:=cameraWaiting;
+  cameraState:=cameraExposing;
+  blevel:=sblevel;
+  cameraSetGain(gain);
+  cameraSetOffset(offset,autoOffset);
+  dlit1:=round(duration*1000)+1000;
+  x:=round(100*(duration*1000)/13);
+  writes($09,x);
+  zad:= round(duration*1000-40);
+  if (zad < 0) then zad:=0;
+  readframe(0,CameraWidth,startY,numY);
+  Result := true;
+end;
+
+//Get camera state, return int result
+function cameraGetCameraState : integer; stdcall; export;
+begin
+  if (not errorWriteFlag) then Result := cameraState
+  else Result := cameraError;
+end;
+
+//Check ImageReady flag
+function cameraGetImageReady : WordBool; stdcall; export;
+begin
+  Result := imageReady;
+end;
+
+//Get back pointer to ASCOM driver
+function cameraGetImage : dword; stdcall; export;
+begin
+  Result := dword(@bufim2);
+end;
+
+//Get camera error state, return bool result
+function cameraGetError : integer; stdcall; export;
+var res : integer;
+begin
+  res:=0;
+  if (errorWriteFlag) then res :=res+2;
+  if (errorReadFlag) then res :=res+1;
+  Result:=res;
+end;
+
+exports cameraConnect;
+exports cameraDisconnect;
+exports cameraIsConnected;
+exports cameraStartExposure;
+exports cameraGetCameraState;
+exports cameraGetImageReady;
+exports cameraGetImage;
+exports cameraGetError;
+
+begin
+  hev := TEvent.Create(nil, false, false, '');
+end.
+
